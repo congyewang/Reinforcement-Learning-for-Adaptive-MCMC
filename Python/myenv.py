0023658fbb33dc6719144d58eafabdf255f5339f
Congye Wang <w.congye2@newcastle.ac.uk>
2023-10-01 19:07:37 +0100

    Add Rao-Blackwell Reward, modify of ESJD NUM to MC

--- Python/myenv.py
+++ Python/myenv.py
@@ -35,8 +35,6 @@ class MyEnv(gym.Env):
         self.store_accetped_status = []
         self.store_action = []
         self.store_reward = []
-        self.store_esjd = []
-        self.store_esjd_error = []
 
     def env_mh(self, theta_curr, policy_func, log_p):
         sigma_curr = policy_func(theta_curr)
@@ -49,10 +47,13 @@ class MyEnv(gym.Env):
         log_q_prop = norm.logpdf(theta_prop, loc=theta_curr, scale=sigma_curr)
         log_q_curr = norm.logpdf(theta_curr, loc=theta_prop, scale=sigma_prop)
 
-        log_alpha = log_p_prop \
-                - log_p_curr \
-                + log_q_curr \
-                - log_q_prop
+        log_alpha = min(
+            log_p_prop \
+            - log_p_curr \
+            + log_q_curr \
+            - log_q_prop,
+            np.array([0.0])
+            )
 
         if np.log(generator.uniform()) < log_alpha:
             theta_curr = theta_prop
@@ -66,7 +67,7 @@ class MyEnv(gym.Env):
         sigma_curr = action
 
         # MCMC Environment
-        state_curr, accepted_status, theta_prop, log_alpha = self.env_mh(theta_curr=self.state, policy_func=policy_func, log_p=self.log_p)
+        state_curr, accepted_status, state_prop, log_alpha = self.env_mh(theta_curr=self.state, policy_func=policy_func, log_p=self.log_p)
 
         # Store
         self.store_state.append(state_curr)
@@ -75,7 +76,7 @@ class MyEnv(gym.Env):
         self.store_log_accetance_rate.append(log_alpha)
 
         # Calculate Reward
-        reward = np.power(np.linalg.norm(self.state - state_curr, 2), 2)
+        reward = np.power(np.linalg.norm(state_curr - state_prop, 2), 2) * np.exp(log_alpha)
         self.store_reward.append(reward)
 
         # Update Iteration Time
@@ -94,7 +95,7 @@ class MyEnv(gym.Env):
             "state": state_curr,
             "accepted_status": accepted_status,
             "reward": reward,
-            "theta_prop": theta_prop
+            "state_prop": state_prop
         }
 
         return state_curr, reward, terminated, truncated, info
@@ -106,14 +107,14 @@ class MyEnv(gym.Env):
         self.store_state.append(self.state)
         self.store_accetped_status.append(True)
         self.store_action.append(np.eye(self.dim))
-        self.store_reward.append(0.0)
+        self.store_reward.append(np.array([0.0]))
         self.store_log_accetance_rate.append(np.array([0.0]))
 
         # Information
         info = {
             "state": self.state,
             "accepted_status": True,
-            "reward": 0.0
+            "reward": np.array([0.0])
         }
 
         # Initialize ESJD Parameters
@@ -124,54 +125,3 @@ class MyEnv(gym.Env):
     def render(self, mode="human"):
         plt.plot(self.store_state)
         plt.show()
-
-    def log_q_proposal(self, x_t_plus_1, x_t, policy_func):
-        sigma_t = policy_func(x_t)
-
-        return norm.logpdf(x_t_plus_1, loc=x_t, scale=sigma_t)
-
-    def log_acceptance_ratio(self, x_t, x_t_plus_1, policy_func):
-        sigma_t = policy_func(x_t)
-        sigma_t_plus_1 = policy_func(x_t_plus_1)
-
-        log_p_t_plus_1 = self.log_p(x_t_plus_1)
-        log_p_t = self.log_p(x_t)
-        log_q_t_plus_1 = norm.logpdf(x_t_plus_1, loc=x_t, scale=sigma_t)
-        log_q_t = norm.logpdf(x_t, loc=x_t_plus_1, scale=sigma_t_plus_1)
-
-        log_alpha = log_p_t_plus_1 \
-                - log_p_t \
-                + log_q_t \
-                - log_q_t_plus_1
-
-        return min(0, log_alpha)
-
-    def log_squared_jump_distance(self, x_t, x_t_plus_1):
-        return np.log(np.power(np.linalg.norm(np.array([x_t]) - np.array([x_t_plus_1]), 2), 2))
-
-    def expected_squared_jump_distance_single_iteration(self, x_t, x_t_plus_1, policy_func):
-        return np.exp(self.log_p(x_t) \
-            + self.log_acceptance_ratio(x_t, x_t_plus_1, policy_func) \
-            + self.log_q_proposal(x_t_plus_1, x_t, policy_func) \
-            + self.log_squared_jump_distance(x_t, x_t_plus_1))
-
-    def quartile(self, loc=0, scale=1, alpha=0.005):
-        self.lower_limit, self.upper_limit = norm.ppf([0.5 * alpha, 1 - 0.5 * alpha], loc=loc, scale=scale)
-
-    def expected_squared_jump_distance(self, policy_func):
-        """
-        Numerical integration to calculate expected squared jump distance
-        """
-        result, error = dblquad(
-            self.expected_squared_jump_distance_single_iteration,
-            self.lower_limit,
-            self.upper_limit,
-            lambda x: self.lower_limit,
-            lambda x: self.upper_limit,
-            args=[policy_func]
-            )
-
-        self.store_esjd.append(result)
-        self.store_esjd_error.append(error)
-
-        # return result, error
