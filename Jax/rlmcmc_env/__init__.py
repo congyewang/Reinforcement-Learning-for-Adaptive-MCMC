ad6a5bb3bc5a8a83478ba8d231c46908b97797b8
Congye Wang <w.congye2@newcastle.ac.uk>
2023-11-14 19:59:07 +0000

    Rename

--- Jax/rlmcmc_env/__init__.py
+++ Jax/rlmcmc_env/__init__.py
@@ -3,7 +3,7 @@ from .rl_mh import RLMHEnv
 
 register(
     id='RLMHEnv-v0',
-    entry_point='rl_mcmc_env.rl_mh:RLMHEnv',
+    entry_point='rlmcmc_env.rl_mh:RLMHEnv',
 )
 
 __all__ = ["RLMHEnv"]
--- SB3/run_DDPG.py
+++ SB3/run_DDPG.py
@@ -1,15 +1,23 @@
+import numpy as np
+from torch import nn
+import gymnasium as gym
+import matplotlib.pyplot as plt
+from stable_baselines3.common.preprocessing import get_flattened_obs_dim
+from stable_baselines3.common.noise import (
+    NormalActionNoise,
+    OrnsteinUhlenbeckActionNoise,
+)
+
+from rlmcmc_env import RLMHEnv
+from rlmcmc_policy.policy import RLMHTD3Policy, RLMHDDPG
+
 import os
 import json
 import pandas as pd
 import bridgestan as bs
 from posteriordb import PosteriorDatabase
 
-import numpy as np
-import matplotlib.pyplot as plt
-import gymnasium as gym
-
 from mcmctoolbox.functoolbox import flat
-from rlmcmc_policy import RLMHTD3Policy, RLMHDDPG
 
 
 # Load DataBase Locally
@@ -17,7 +25,7 @@ pdb_path = os.path.join(os.getcwd(), "../Python/posteriordb/posterior_database")
 my_pdb = PosteriorDatabase(pdb_path)
 
 # Load Dataset
-posterior = my_pdb.posterior("test-AnnulusGaussianMixture-test-AnnulusGaussianMixture")
+posterior = my_pdb.posterior("test-SimpleEggBox-test-SimpleEggBox")
 stan = posterior.model.stan_code_file_path()
 data = json.dumps(posterior.data.values())
 model = bs.StanModel.from_stan_file(stan, data)
@@ -40,36 +48,48 @@ linv = np.linalg.inv(np.cov(gs_chains))
 
 # Extract log-P-pdf and its gradient
 log_p = model.log_density
-# grad_log_p = lambda x: model.log_density_gradient(x)[1]
-# hess_log_p = lambda x: model.log_density_hessian(x)[2]
 
 
-rl_mh_env = gym.make(
-    "RLMHEnv-v0", log_target_pdf=log_p, sample_dim=2, total_timesteps=10_000
+total_timesteps = 5_000
+rlmh_env = gym.make(
+    "RLMHEnv-v0", log_target_pdf=log_p, sample_dim=2, total_timesteps=total_timesteps
 )
 
 
-model = RLMHDDPG(RLMHTD3Policy, rl_mh_env, verbose=1, learning_starts=-1, device="cpu")
-
-model.learn(total_timesteps=10_000, progress_bar=True)
-
-
-state_list = np.array([i for i in rl_mh_env.store_state]).reshape(
-    -1, rl_mh_env.sample_dim
+policy_kwargs = dict(
+    net_arch=dict(pi=[64, 64, 64], qf=[64, 64, 64]),
+    activation_fn=nn.Softplus,
+    n_critics=1,
 )
 
 
-# Trace Plot
-plt.plot(
-    state_list[:, 0], state_list[:, 1], "o-", linewidth=0.5, markersize=1.5, alpha=0.5
+# The noise objects for DDPG
+n_actions = rlmh_env.action_space.shape[-1]
+action_noise = NormalActionNoise(
+    mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions)
 )
-plt.title("Trace Plot")
-plt.xlabel("Steps")
-plt.ylabel("State")
+
+
+model = RLMHDDPG(
+    RLMHTD3Policy,
+    rlmh_env,
+    seed=1234,
+    policy_kwargs=policy_kwargs,
+    learning_starts=-1,
+    batch_size=64,
+    train_freq=(2, "step"),
+    action_noise=action_noise,
+    device="cpu",
+    verbose=2,
+)
+
+model.learn(total_timesteps=total_timesteps, progress_bar=True)
+
+
+state_list = np.array([i for i in rlmh_env.store_state]).reshape(
+    -1, get_flattened_obs_dim(rlmh_env.observation_space)
+)
+
+
+plt.plot(state_list[:, 0], state_list[:, 1], "o-", alpha=0.1)
 plt.show()
-
-# Acceptance Rate
-print(np.sum(rl_mh_env.store_accetped_status) / len(rl_mh_env.store_accetped_status))
-
-# Reward
-plt.plot(np.array(rl_mh_env.store_reward))
