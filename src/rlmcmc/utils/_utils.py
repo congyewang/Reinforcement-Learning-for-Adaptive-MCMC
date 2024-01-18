19f601a59384b1b09e97d7e552fd149bc1adf4dc
Congye Wang <w.congye2@newcastle.ac.uk>
2024-01-18 20:45:56 +0000

    Add nearestPD

--- src/rlmcmc/utils/_utils.py
+++ src/rlmcmc/utils/_utils.py
@@ -78,3 +78,76 @@ class Toolbox:
             return env
 
         return thunk
+
+    @classmethod
+    def nearestPD(cls, A):
+        """
+        Find the nearest positive-definite matrix to input
+        A Python/Numpy port of John D'Errico's `nearestSPD` MATLAB code [1], which
+        credits [2].
+
+        [1] https://www.mathworks.com/matlabcentral/fileexchange/42885-nearestspd
+        [2] N.J. Higham, "Computing a nearest symmetric positive semidefinite
+        matrix" (1988): https://doi.org/10.1016/0024-3795(88)90223-6
+        """
+
+        B = (A + A.T) / 2
+        _, s, V = np.linalg.svd(B)
+
+        H = np.dot(V.T, np.dot(np.diag(s), V))
+
+        A2 = (B + H) / 2
+
+        A3 = (A2 + A2.T) / 2
+
+        if cls.isPD(A3):
+            return A3
+
+        spacing = np.spacing(np.linalg.norm(A))
+        # The above is different from [1]. It appears that MATLAB's `chol` Cholesky
+        # decomposition will accept matrixes with exactly 0-eigenvalue, whereas
+        # Numpy's will not. So where [1] uses `eps(mineig)` (where `eps` is Matlab
+        # for `np.spacing`), we use the above definition. CAVEAT: our `spacing`
+        # will be much larger than [1]'s `eps(mineig)`, since `mineig` is usually on
+        # the order of 1e-16, and `eps(1e-16)` is on the order of 1e-34, whereas
+        # `spacing` will, for Gaussian random matrixes of small dimension, be on
+        # othe order of 1e-16. In practice, both ways converge, as the unit test
+        # below suggests.
+        I = np.eye(A.shape[0])
+        k = 1
+        while not isPD(A3):
+            mineig = np.min(np.real(np.linalg.eigvals(A3)))
+            A3 += I * (-mineig * k**2 + spacing)
+            k += 1
+
+        return A3
+
+    @classmethod
+    def isPD(cls, B):
+        """
+        Returns true when input is positive-definite, via Cholesky, det, and _PSD from scipy.
+        """
+        try:
+            _ = np.linalg.cholesky(B)
+            res_cholesky = True
+        except np.linalg.LinAlgError:
+            res_cholesky = False
+
+        try:
+            assert np.linalg.det(B) > 0, "Determinant is negative"
+            res_det = True
+        except AssertionError:
+            res_det = False
+
+        try:
+            _PSD(B, allow_singular=False)
+            res_PSD = True
+        except Exception as e:
+            if re.search("[Pp]ositive", str(e)):
+                return False
+            else:
+                raise
+
+        res = res_cholesky and res_det and res_PSD
+
+        return res
