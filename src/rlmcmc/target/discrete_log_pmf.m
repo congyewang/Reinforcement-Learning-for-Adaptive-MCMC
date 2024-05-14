61cd4a3efdab23640cd2320755bb72ddd92d9afe
Congye Wang <congye.wang@outlook.com>
2024-05-14 23:13:36 +0100

    Add demo target funcs

---
+++ src/rlmcmc/target/discrete_log_pmf.m
@@ -0,0 +1,13 @@
+function res = discrete_log_pmf(x)
+
+if x >= -4 && x <= -3
+    res = log(0.25);
+elseif x >= -1 && x <= 1
+    res = log(0.5);
+elseif x >= 3 && x <= 4
+    res = log(0.25);
+else
+    res = -realmax;
+end
+
+end
---
+++ src/rlmcmc/target/skew_normal_logpdf.m
@@ -0,0 +1,28 @@
+function logpdf = skew_normal_logpdf(x, alpha, mu, var)
+
+if nargin < 2
+    alpha = 2;
+end
+if nargin < 3
+    mu = 0;
+end
+if nargin < 4
+    var = 1;
+end
+
+if var <= 0
+    error('var must be positive.');
+end
+
+std = sqrt(var);
+
+z = (x - mu) / std;
+
+log_phi = -0.5 * log(2 * pi) - 0.5 * z.^2;
+Phi_alpha_z = 0.5 * (1 + erf(alpha * z / sqrt(2)));
+
+Phi_alpha_z(Phi_alpha_z == 0) = realmin;
+
+logpdf = log(2) - log(std) + log_phi + log(Phi_alpha_z);
+
+end
