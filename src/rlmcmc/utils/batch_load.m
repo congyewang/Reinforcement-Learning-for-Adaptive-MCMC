2903b2772a6613537ddf23b3ff9600b875700eb6
Congye Wang <congye.wang@outlook.com>
2024-05-20 13:15:45 +0100

    Encapsulate as a func

---
+++ src/rlmcmc/utils/batch_load.m
@@ -0,0 +1,25 @@
+function [] = batch_load(free_load)
+
+if nargin < 1
+    free_load = false;
+end
+
+lib_super_path = "../../experiments/trails";
+
+if free_load == false
+    add_log_target_pdf_lib(lib_super_path);
+    add_lib_path(lib_super_path);
+
+    t = search_model_table(lib_super_path);
+
+    origin_pdb_condition = cellfun(@isempty, strfind(t.name, 'test_'));
+    origin_t = t(origin_pdb_condition, :);
+
+    sorted_origin_t = sortrows(origin_t, 'dim');
+
+    disp(sorted_origin_t);
+else
+    free_log_target_pdf_lib(lib_super_path);
+end
+
+end
