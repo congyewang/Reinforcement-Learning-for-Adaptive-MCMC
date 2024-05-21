bd63f94f88bff5237e58403c6805325c69ec0968
Congye Wang <congye.wang@outlook.com>
2024-05-21 21:33:57 +0100

    Delete personal info

--- experiments/template/template.run-mala.sh
+++ experiments/template/template.run-mala.sh
@@ -3,7 +3,6 @@
 
 module load GCC/6.3.0-2.27
 
-source /nobackup/c2029946/Software/miniconda3/bin/activate rl
 python mala.py
 
 echo Finishing job
--- experiments/template/template.run-nuts.sh
+++ experiments/template/template.run-nuts.sh
@@ -3,7 +3,6 @@
 
 module load GCC/6.3.0-2.27
 
-source /nobackup/c2029946/Software/miniconda3/bin/activate rl
 python nuts.py
 
 echo Finishing job
